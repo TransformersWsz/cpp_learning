ecbc0230708e8486e87ab7e690f4c465fb36b8cf
TransformersWsz <3287124026@qq.com>
2022-12-15 00:45:00 +0800

    add namespace

---
+++ namespace/myutils/head.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+namespace utils {
+    void print(int i) {
+        std::cout << i << std::endl;
+    }
+}
---
+++ namespace/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "myutils/head.h"
+
+int main() {
+    utils::print(123);
+    // return 0;
+}
